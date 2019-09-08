43bd0c62820c3e338a43fd6bb688d90ea2aa0e20
ArtemBoreyko <sreadbe44@gmail.com>
2019-09-09 00:37:32 +0300

    Correction

--- Tax and fee calculator(for Ukraine)/src/com/company/Main.java
+++ Tax and fee calculator(for Ukraine)/src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void comparator(){
+    public void comparator(){
         double sum = input(), tax, fee;
         if(sum < 100 && sum > 0) {
             System.out.println("This sum can not be taxed!");
@@ -19,26 +19,27 @@ public class Main {
         else System.out.println("Incorrect input!");
     }
 
-    public static double input(){
+    public double input(){
         Scanner in = new Scanner(System.in);
         System.out.println("Input the sum in euros: ");
         double sum = in.nextDouble();
         return sum;
     }
 
-    public static double tax(double sum){
+    public double tax(double sum){
         double tax;
         tax = sum  * 0.2;
         return tax;
     }
 
-    public static double fee(double sum){
+    public double fee(double sum){
         double fee;
         fee = sum * 0.1;
         return fee;
     }
 
     public static void main(String[] args) {
-        comparator();
+        Main main = new Main();
+        main.comparator();
     }
 }
