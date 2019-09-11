8008d7fedb60e7c51e8d05beb315304462173f72
ArtemBoreyko <sreadbe44@gmail.com>
2019-09-11 17:01:45 +0300

    Correction

--- Work_with_stringbuffer/src/com/company/Main.java
+++ Work_with_stringbuffer/src/com/company/Main.java
@@ -14,17 +14,24 @@ import java.lang.String;
             main.output(str, sb);
         }
 
-        public void update(String str, StringBuffer sb){
-            int index_p = str.indexOf('р') ;
-            if(index_p == -1) {
+        public void update(String str, StringBuffer sb) {
+            int index_p = str.indexOf('р');
+            if (index_p == -1) {
                 int index_P = str.indexOf('Р');
-                int index_a = str.indexOf('а', index_P);
-                if(index_P + 1 == index_a){
-                    sb.deleteCharAt(index_a);
-                    sb.insert(index_a, 'о');
-                }
+                replace(str, sb, index_P);
+            } else if(index_p == 0) {
+                replace(str, sb, index_p);
             }
         }
+
+        private void replace(String str, StringBuffer sb, int index_p) {
+            int index_a = str.indexOf('а', index_p);
+            if (index_p + 1 == index_a) {
+                sb.deleteCharAt(index_a);
+                sb.insert(index_a, 'о');
+            }
+        }
+
 
         public void output(String str, StringBuffer sb){
             System.out.println(str);
