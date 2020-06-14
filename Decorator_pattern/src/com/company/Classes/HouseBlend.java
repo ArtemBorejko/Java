177a793f2ca222dea9348d8c6bb2aeea64d49b55
Artem Borejko <sreadbe44@gmail.com>
2020-06-14 16:33:07 +0300

    Added functionality.

---
+++ Decorator_pattern/src/com/company/Classes/HouseBlend.java
@@ -0,0 +1,11 @@
+package com.company.Classes;
+
+public class HouseBlend extends Beverage {
+    public HouseBlend(){
+        description = "House blend";
+    }
+
+    public double cost(){
+        return .99;
+    }
+}
--- Decorator_pattern/src/com/company/Main.java
+++ Decorator_pattern/src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import com.company.Classes.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " " + beverage.cost() + "$");
+
+        Beverage beverage1 = new HouseBlend();
+        beverage1 = new Mocha(beverage1);
+        beverage1 = new Whip(beverage1);
+
+        System.out.println(beverage1.getDescription() + " " + beverage1.cost() + "$");
     }
 }
